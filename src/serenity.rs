0a14b52aeb11a8965c634632c5254f18fbe24c79
CrySteRz <blidaruionutz@gmail.com>
2024-08-07 20:55:39 +0000

    Moving from messages to interactions

--- src/serenity.rs
+++ src/serenity.rs
@@ -28,16 +28,23 @@ impl SerenityInit for ClientBuilder {
     }
 }
 
-#[macro_export]
-macro_rules! get_handler_from_serenity_mutable {
-    ($ctx: expr,$msg: expr,$reference: ident) => {
+macro_rules! get_handler_from_interaction_mutable {
+    ($ctx: expr, $interaction: expr, $reference: ident) => {
         let r = $ctx.data.read().await;
+        
         // Get the GuildID
-        let guild = $msg.guild(&$ctx.cache).unwrap();
-        let guild_id = guild.id;
+        let guild_id = match $interaction.guild_id {
+            Some(gid) => gid,
+            None => {
+                eprintln!("No guild ID found in interaction");
+                return;
+            }
+        };
+
         // Get the charcoal manager from the serenity typemap
         let manager = r.get::<CharcoalKey>();
         let mut mx = manager.unwrap().lock().await;
+        
         // Get the PlayerObject
         let mut players = mx.players.write().await;
         $reference = players.get_mut(&guild_id.to_string());
@@ -45,15 +52,23 @@ macro_rules! get_handler_from_serenity_mutable {
 }
 
 #[macro_export]
-macro_rules! get_handler_from_serenity {
-    ($ctx: expr,$msg: expr,$reference: ident) => {
+macro_rules! get_handler_from_interaction {
+    ($ctx: expr, $interaction: expr, $reference: ident) => {
         let r = $ctx.data.read().await;
+        
         // Get the GuildID
-        let guild = $msg.guild(&$ctx.cache).unwrap();
-        let guild_id = guild.id;
+        let guild_id = match $interaction.guild_id {
+            Some(gid) => gid,
+            None => {
+                eprintln!("No guild ID found in interaction");
+                return;
+            }
+        };
+
         // Get the charcoal manager from the serenity typemap
         let manager = r.get::<CharcoalKey>();
         let mut mx = manager.unwrap().lock().await;
+        
         // Get the PlayerObject
         let players = mx.players.read().await;
         $reference = players.get(&guild_id.to_string());
